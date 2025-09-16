8bc92d705aa928391f5f85377c02292cc0a933c6
Diana <146003147+dianahuamant@users.noreply.github.com>
2025-09-16 13:48:44 -0500

    Update driver_factory.py

--- UI/utils/driver_factory.py
+++ UI/utils/driver_factory.py
@@ -1,16 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
-import tempfile
 
 def create_driver(headless: bool = False):
     options = webdriver.ChromeOptions()
 
+    # Configs necesarias en Linux CI
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--window-size=1920,1080")
-    options.add_argument("--incognito")
+    options.add_argument("--disable-gpu")  # evita errores en headless en Linux
+    options.add_argument("--disable-software-rasterizer")
 
+    # Forzar tamaño de ventana
+    options.add_argument("--window-size=1920,1080")
+
+    # Si es headless
     if headless:
         options.add_argument("--headless=new")
 
@@ -21,4 +25,3 @@ def create_driver(headless: bool = False):
 
     driver.implicitly_wait(5)
     return driver
-
