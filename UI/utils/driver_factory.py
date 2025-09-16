082756f966c2ab7b5354cbe45e93eb6ee17e21eb
Diana <146003147+dianahuamant@users.noreply.github.com>
2025-09-16 14:00:42 -0500

    Update driver_factory.py

--- UI/utils/driver_factory.py
+++ UI/utils/driver_factory.py
@@ -1,15 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+import tempfile
+
 
 def create_driver(headless: bool = False):
     options = webdriver.ChromeOptions()
 
-    # Configs necesarias en Linux CI
+    # Configs necesarias en Linux CI/CD
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--disable-gpu")  # evita errores en headless en Linux
+    options.add_argument("--disable-gpu")  # evita errores gráficos
     options.add_argument("--disable-software-rasterizer")
+
+    # Perfil temporal único (evita error "user data directory in use")
+    options.add_argument(f"--user-data-dir={tempfile.mkdtemp()}")
 
     # Forzar tamaño de ventana
     options.add_argument("--window-size=1920,1080")
