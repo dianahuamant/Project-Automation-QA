f7dd3e7adcb3c9836af82a88297b6151b12384df
Diana <146003147+dianahuamant@users.noreply.github.com>
2025-09-16 13:35:35 -0500

    Update driver_factory.py

--- UI/utils/driver_factory.py
+++ UI/utils/driver_factory.py
@@ -6,11 +6,13 @@ from webdriver_manager.chrome import ChromeDriverManager
 def create_driver(headless: bool = False):
     options = webdriver.ChromeOptions()
 
-    if headless:
-        options.add_argument("--headless=new")
-
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")
     options.add_argument("--incognito")
+
+    if headless:
+        options.add_argument("--headless=new")
 
     driver = webdriver.Chrome(
         service=ChromeService(ChromeDriverManager().install()),
