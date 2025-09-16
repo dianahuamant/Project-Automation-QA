50bb011b8b72e6036db9f83cedead772ef06a9c5
Diana <146003147+dianahuamant@users.noreply.github.com>
2025-09-16 13:42:12 -0500

    Update driver_factory.py

--- UI/utils/driver_factory.py
+++ UI/utils/driver_factory.py
@@ -8,9 +8,6 @@ def create_driver(headless: bool = False):
 
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-
-    options.add_argument(f'--user-data-dir={tempfile.mkdtemp()}')
-
     options.add_argument("--window-size=1920,1080")
     options.add_argument("--incognito")
 
