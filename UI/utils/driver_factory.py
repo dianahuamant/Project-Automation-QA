3be4382b5980c506001fc983546920397c84594a
Diana <146003147+dianahuamant@users.noreply.github.com>
2025-09-16 13:38:11 -0500

    Update driver_factory.py

--- UI/utils/driver_factory.py
+++ UI/utils/driver_factory.py
@@ -1,13 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
-
+import tempfile
 
 def create_driver(headless: bool = False):
     options = webdriver.ChromeOptions()
 
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+
+    options.add_argument(f'--user-data-dir={tempfile.mkdtemp()}')
+
     options.add_argument("--window-size=1920,1080")
     options.add_argument("--incognito")
 
@@ -21,3 +24,4 @@ def create_driver(headless: bool = False):
 
     driver.implicitly_wait(5)
     return driver
+
