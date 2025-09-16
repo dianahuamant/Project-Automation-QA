5033d01f25cdb426cc4392f1d711d704272d1acf
Diana <146003147+dianahuamant@users.noreply.github.com>
2025-09-16 13:31:43 -0500

    Update conftest.py

--- UI/tests/conftest.py
+++ UI/tests/conftest.py
@@ -1,6 +1,7 @@
 from time import sleep
 import pytest
 from ..utils.driver_factory import create_driver
+import subprocess
 
 def pytest_addoption(parser):
     parser.addoption(
@@ -11,6 +12,7 @@ def pytest_addoption(parser):
 
 @pytest.fixture
 def driver(request):
+    subprocess.run(["pkill", "-f", "chromedriver"])
     headless = request.config.getoption("--headless")
     driver = create_driver(headless=headless)
     yield driver
