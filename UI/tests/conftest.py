cfb45c7ac97553b5343d634badb475677f81fde5
Diana <146003147+dianahuamant@users.noreply.github.com>
2025-09-16 14:01:53 -0500

    Update conftest.py

--- UI/tests/conftest.py
+++ UI/tests/conftest.py
@@ -1,7 +1,8 @@
 from time import sleep
 import pytest
 import subprocess
-from UI.utils.driver_factory import create_driver  
+from UI.utils.driver_factory import create_driver
+
 
 def pytest_addoption(parser):
     parser.addoption(
@@ -10,13 +11,19 @@ def pytest_addoption(parser):
         help="Ejecutar pruebas en modo headless (sin interfaz de usuario)"
     )
 
+
 @pytest.fixture
 def driver(request):
     # Evitar procesos colgados de Chrome/Driver
-    subprocess.run(["pkill", "-f", "chromedriver"], check=False)
+    subprocess.run(["pkill", "-9", "-f", "chromedriver"], check=False)
+    subprocess.run(["pkill", "-9", "-f", "chrome"], check=False)
 
     headless = request.config.getoption("--headless")
+
     driver = create_driver(headless=headless)
+
     yield driver
-    sleep(3)
+
+    # Espera breve antes de cerrar (para que se guarden logs/screenshots si aplicara)
+    sleep(2)
     driver.quit()
